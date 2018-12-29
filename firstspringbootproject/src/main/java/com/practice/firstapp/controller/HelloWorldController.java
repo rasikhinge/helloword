44973c51a00d483f041650ce810b7c57054ca049
unknown <rasik.hinge4@gmail.com>
2018-12-29 15:28:22 +0530

    added controller for web

---
+++ firstspringbootproject/src/main/java/com/practice/firstapp/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.practice.firstapp.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping(value="/hello",method= RequestMethod.GET)
+    public String sayHello(){
+        return "Hello";
+    }
+}
