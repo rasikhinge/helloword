0e2160c7e198c2e974919cc1849da7874c4ae59f
unknown <rasik.hinge4@gmail.com>
2019-01-01 12:56:30 +0530

    changed package to war file

--- firstspringbootproject/src/main/java/com/practice/firstapp/HelloWorld.java
+++ firstspringbootproject/src/main/java/com/practice/firstapp/HelloWorld.java
@@ -5,16 +5,23 @@ import com.practice.firstapp.controller.HelloController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @Component
-public class HelloWorld {
+public class HelloWorld extends SpringBootServletInitializer {
 
     @Autowired
     private Greet greet1;
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HelloWorld.class);
+    }
 
     public static void main(String[] args) {
 
